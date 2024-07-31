449d2888ddef552cddcbaa71bfaf087e6451c256
Janko Gasic <janko.gasic@deepintent.com>
2024-07-31 19:57:33 +0200

    aaa

--- skripta.py
+++ skripta.py
@@ -1,7 +1,8 @@
+'''Module docstring'''
 from metaflow import step, FlowSpec
 
+'''Class docstring'''
 class HelloWorld(FlowSpec):
-    
     @step
     def start(self):
         self.data = "Hello World"
@@ -10,7 +11,6 @@ class HelloWorld(FlowSpec):
     @step
     def end(self):
         print("the shape of data is still: %", self.data)
-
 
 if __name__ == "__main__":
     HelloWorld()
