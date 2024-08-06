0c3929d84460508d97e00183b0ea49a9481e46f2
Janko Gasic <janko.gasic@deepintent.com>
2024-08-06 19:52:26 +0200

    aaaaaa

--- skripta.py
+++ skripta.py
@@ -1,16 +1,27 @@
-"""Module docstring"""
-from metaflow import step, FlowSpec
+from metaflow import FlowSpec, step
+import time
 
-"""Class docstring"""
-class HelloWorld(FlowSpec):
+class ForeachFlow(FlowSpec):
     @step
     def start(self):
-        self.data = "Hello World"
+        self.titles = ["Stranger Things", "House of Cards", "Narcos","Aaa","Bbbb","Ddddd","Cccc","Eeee","Fffff","Gggg"]
+        self.next(self.a, foreach="titles")
+
+    @step
+    def a(self):
+        self.title = "%s processed" % self.input
+        time.sleep(5)
+        self.next(self.join)
+
+    @step
+    def join(self, inputs):
+        self.results = [input.title for input in inputs]
         self.next(self.end)
 
     @step
     def end(self):
-        print("the shape of data is still: %", self.data)
+        print("\n".join(self.results))
+
 
 if __name__ == "__main__":
-    HelloWorld()
+    ForeachFlow()
